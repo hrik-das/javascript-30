739e33f6b2d1f30c578b04d8aeff239170fa67ef
hrik-das <emptynull01@gmail.com>
2024-04-02 21:46:37 +0530

    New Project Added

---
+++ Clock/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+	
+function setDate(){
+    const date = new Date();
+    const second = date.getSeconds();
+    const secondDegrees = (90 + (second / 60) * 360);
+    const minute = date.getMinutes();
+    const minuteDegrees = (90 + (minute / 60) * 360);
+    const hour = date.getHours();
+    const hourDegrees = (90 + (hour / 12) * 360 + (minute / 12 / 60) * 360);
+    if(secondDegrees === 90) secondHand.style.transition = "all 0s";
+    else secondHand.style.transition = "all 0.05s cubic-bezier(0.9, 0.54, 0.26, 1.68)";
+    if (minuteDegrees === 90) minHand.style.transition = "all 0s";
+    else minHand.style.transition = "all 0.1s cubic-bezier(0.9, 0.54, 0.26, 1.68)";
+    secondHand.style.transform = `rotate(${ secondDegrees }deg)`;
+    minHand.style.transform = `rotate(${ minuteDegrees }deg)`;
+    hourHand.style.transform = `rotate(${ hourDegrees }deg)`;
+    // console.log(`${hour}:${minute}:${second} - ${hourDegrees}:${minuteDegrees}:${secondDegrees}` );
+}
+
+setInterval(setDate, 1000);
